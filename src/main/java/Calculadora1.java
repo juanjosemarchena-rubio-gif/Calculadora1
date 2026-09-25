0634d79f0ba113c74aa7816e6714df28105f4337
Juan José Marchena <Juan Jos Marchena@DESKTOP-NQO1TN8>
2026-09-25 16:22:21 +0200

    Bienvenida

--- src/main/java/Calculadora1.java
+++ src/main/java/Calculadora1.java
@@ -14,7 +14,7 @@ public class Calculadora {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+System.out.print("Bienvenido");
         System.out.print("Introduce el primer número: ");
         double num1 = scanner.nextDouble();
 
